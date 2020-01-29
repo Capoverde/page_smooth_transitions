b7ad47cf26908132c02bd2d7839ae32be6172ca9
Piotr <piotr.slobinski@gmail.com>
2020-01-29 13:38:56 +0100

    cała reszta

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
+import Highway from '@dogstudio/highway';
+import Fade from './transitions';
 import { navchange } from '/js/navBg'
+
+
+const H = new Highway.Core({
+  transitions: {
+    default: Fade
+  }
+});
+
 
 // event listener:
 document.addEventListener('scroll', navchange)
--- js/navBg.js
+++ js/navBg.js
@@ -1,9 +1,5 @@
 const nav = document.querySelector('.main__nav')
-const body = document.querySelector('body')
 
-const navchange = e => {
-  // console.log(scrollTop)
-  window.pageYOffset > 50 ? nav.classList.add('navBg') : nav.classList.remove('navBg')
-}
+const navchange = e => window.pageYOffset >= 50 ? nav.classList.add('navBg') : nav.classList.remove('navBg')
 
 export { navchange }
